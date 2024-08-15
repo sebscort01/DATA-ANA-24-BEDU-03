42b98d1b86a962f0ba4e34261ae92f7a4a1ed6b5
Sebastian Cortés <Jorgecortes1506@gmail.com>
2024-08-14 21:55:31 -0600

    last update

--- utils_distribuciones.py
+++ utils_distribuciones.py
@@ -3,5 +3,22 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.stats import skew, kurtosis
 
+'''
+Documentacion de una funcion o clase
+'''
 def generar_distribucion(data, titulo):
-    asimetria = 
+    asimetria = skew(data)
+    curtosis = kurtosis(data)
+
+    print(f'{titulo}: Asimetria: {asimetria:.2f}, Curtosis: {curtosis:.2f}')
+
+    # Histograma
+    sns.histplot(data, kde=False)
+
+    plt.title(titulo)
+    plt.xlabel('Valor')
+    plt.ylabel('Frecuencia')
+    plt.grid(True)
+    plt.show()
+
+print('todo bien')
